54d6085c12ffaafe2cff457074301142fbe67f73
emilyyvonne <148805367+emilyyvonne@users.noreply.github.com>
2024-06-01 11:20:34 -0400

    updated target

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ export default function App() {
   return (
     <footer>
       This project was coded by Emily Brisbin, is
-      <a href="https://github.com/emilyyvonne/weatherAppFlex" target="_blank">
+      <a
+        href="https://github.com/emilyyvonne/weatherAppFlex"
+        rel="noreferrer"
+        target="_blank"
+      >
         {" "}
         open-sourced on GitHub{" "}
       </a>
       and
-      <a href="https://comfy-otter-077cbe.netlify.app/" target="_blank">
+      <a
+        href="https://comfy-otter-077cbe.netlify.app/"
+        rel="noreferrer"
+        target="_blank"
+      >
         {" "}
         hosted on Netlify
       </a>
